444555369b51f02c4c205287b5ca8132cb7c2d31
Rohan Bhansali <rohanmbhansali@gmail.com>
2020-05-04 18:14:03 -0400

    Add files via upload

---
+++ testmodel.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 13:04:03 2020
+
+@author: Avi
+"""
+import pandas as pd
+from glob import glob
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+xray_full = pd.read_csv('C:/Users/Avi/Desktop/Data/Data_Entry_2017.csv')
+xray_data = xray_full[0:14999]
+
+image_glob = glob('C:/Users/Avi/Desktop/Data/images*/images/*.png')
+full_img_paths = {os.path.basename(x): x for x in image_glob}
+xray_data['full_path'] = xray_data['Image Index'].map(full_img_paths.get)
+
+dummy_labels = ['Atelectasis', 'Consolidation', 'Infiltration', 'Pneumothorax', 'Edema', 'Emphysema', 'Fibrosis', 'Effusion', 'Pneumonia', 'Pleural_Thickening', 
+'Cardiomegaly', 'Nodule', 'Mass', 'Hernia'] 
+
+for label in dummy_labels:
+    xray_data[label] = xray_data['Finding Labels'].map(lambda result: 1.0 if label in result else 0)
+
+xray_data['target_vector'] = xray_data.apply(lambda target: [target[dummy_labels].values], 1).map(lambda target: target[0])
+
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(xray_data, test_size = 0.2, random_state = 2000)
+
+from keras.preprocessing.image import ImageDataGenerator
+data_gen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True)
+
+def flow_from_dataframe(img_data_gen, in_df, path_col, y_col, **dflow_args):
+    base_dir = os.path.dirname(in_df[path_col].values[0])
+    print('## Ignore next message from keras, values are replaced anyways')
+    df_gen = img_data_gen.flow_from_directory(base_dir, 
+                                     class_mode = 'sparse',
+                                    **dflow_args)
+    df_gen.filenames = in_df[path_col].values
+    df_gen.classes = np.stack(in_df[y_col].values)
+    df_gen.samples = in_df.shape[0]
+    df_gen.n = in_df.shape[0]
+    df_gen._set_index_array()
+    df_gen.directory = '' # since we have the full path
+    print('Reinserting dataframe: {} images'.format(in_df.shape[0]))
+    return df_gen
+
+image_size = (128, 128) # image re-sizing target
+train_gen = flow_from_dataframe(data_gen, train_set, path_col = 'full_path', y_col = 'target_vector', target_size = image_size, color_mode = 'grayscale', batch_size = 8)
+valid_gen = flow_from_dataframe(data_gen, test_set, path_col = 'full_path', y_col = 'target_vector', target_size = image_size, color_mode = 'grayscale', batch_size = 32)
+
+# define test sets
+test_X, test_Y = next(flow_from_dataframe(data_gen, test_set, path_col = 'full_path', y_col = 'target_vector', target_size = image_size, color_mode = 'grayscale', 
+                                          batch_size = 8))
+
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
+
+# Create CNN model
+# Will use a combination of convolutional, max pooling, and dropout layers for this purpose
+model = Sequential()
+
+model.add(Conv2D(filters = 8, kernel_size = 3, padding = 'same', activation = 'relu', input_shape = test_X.shape[1:]))
+model.add(MaxPooling2D(pool_size = 2))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters = 16, kernel_size = 3, padding = 'same', activation = 'relu'))
+model.add(MaxPooling2D(pool_size = 2))
+model.add(Dropout(0.2))
+          
+model.add(Conv2D(filters = 32, kernel_size = 3, padding = 'same', activation = 'relu'))
+model.add(MaxPooling2D(pool_size = 2))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters = 64, kernel_size = 3, padding = 'same', activation = 'relu'))
+model.add(MaxPooling2D(pool_size = 2))
+model.add(Dropout(0.2))
+          
+model.add(Conv2D(filters = 128, kernel_size = 3, padding = 'same', activation = 'relu'))
+model.add(MaxPooling2D(pool_size = 3))
+model.add(Dropout(0.2))
+
+# add in fully connected dense layers to model, then output classifiction probabilities using a softmax activation function
+model.add(Flatten())
+model.add(Dense(500, activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(len(dummy_labels), activation = 'softmax'))
+
+# compile model, run summary
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+from keras.callbacks import ModelCheckpoint
+
+checkpointer = ModelCheckpoint(filepath='weights.best.{epoch:02d}-{val_loss:.2f}.hdf5', verbose=1, save_best_only = True)
+callbacks_list = [checkpointer]
+model.fit_generator(generator = train_gen, steps_per_epoch = 20, epochs = 1, callbacks = callbacks_list, validation_data = (test_X, test_Y))
+
+quick_model_predictions = model.predict(test_X, batch_size = 64, verbose = 1)
+
+from sklearn.metrics import roc_curve, auc
+
+# create plot
+fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
+for (i, label) in enumerate(dummy_labels):
+    fpr, tpr, thresholds = roc_curve(test_Y[:,i].astype(int), quick_model_predictions[:,i])
+    c_ax.plot(fpr, tpr, label = '%s (AUC:%0.2f)'  % (label, auc(fpr, tpr)))
+
+# Set labels for plot
+c_ax.legend()
+c_ax.set_xlabel('False Positive Rate')
+c_ax.set_ylabel('True Positive Rate')
+fig.savefig('quick_trained_model.png')
